146a98e10e522954648f4554610ed8244d633201
Kapil Sarma <sarmakapil1818@gmail.com>
2025-01-18 16:03:44 +0530

    added delete button

--- App.tsx
+++ App.tsx
@@ -1,12 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity, Alert } from 'react-native';
 import { theme } from './theme';
 
 export default function App() {
+
+  const handleDelete = () => {
+    Alert.alert('Delete', 'Are you sure you want to delete this item?', [
+      { text: 'Cancel', style: 'cancel' },
+      { text: 'Delete', onPress: () => {console.log('deleted') }, style: 'destructive'},
+    ]);
+  }
+
   return (
     <View style={styles.container}>
       <View style ={styles.itemContainer}>
         <Text style={styles.itemText}>Coffee</Text>
+      <TouchableOpacity style={styles.button} onPress={handleDelete} activeOpacity={0.7}>
+        <Text style={styles.buttonText}>Delete</Text>
+      </TouchableOpacity>
       </View>
       <StatusBar style="auto" />
     </View>
@@ -16,10 +27,13 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: theme.colorWhite,
     justifyContent: 'center',
   },
   itemContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
     borderBottomWidth: 1,
     borderBottomColor: theme.colorCerulean,
     paddingVertical: 8,
@@ -29,5 +43,15 @@ const styles = StyleSheet.create({
   itemText: {
     fontSize: 18,
     fontWeight: 200,
+  },
+  button: {
+    backgroundColor: theme.colorBlack,
+    padding: 8,
+    borderRadius: 6,
+  },
+  buttonText: {
+    color: theme.colorWhite,
+    fontWeight: 'bold',
+    textTransform: 'uppercase',
   }
 });
--- theme.ts
+++ theme.ts
@@ -1,3 +1,5 @@
 export const theme = {
   colorCerulean: '#1a759f',
+  colorWhite: '#fff',
+  colorBlack: '#000',
 }
